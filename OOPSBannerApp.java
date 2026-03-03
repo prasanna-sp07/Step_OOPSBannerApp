9e42c57754137549a05a5bc4df983922b40f4608
prasanna-sp07 <prasanna.sp2540@gmail.com>
2026-03-03 21:40:34 +0530

    Added UC7

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -3,16 +3,40 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] o = buildO();
-        String[] p = buildP();
-        String[] s = buildS();
+        CharacterPatternMap o = new CharacterPatternMap('O', buildO());
+        CharacterPatternMap p = new CharacterPatternMap('P', buildP());
+        CharacterPatternMap s = new CharacterPatternMap('S', buildS());
+
+        String[] oPattern = o.getPattern();
+        String[] pPattern = p.getPattern();
+        String[] sPattern = s.getPattern();
 
         for (int i = 0; i < 7; i++) {
-            System.out.println(o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i]);
+            System.out.println(
+                oPattern[i] + "  " +
+                oPattern[i] + "  " +
+                pPattern[i] + "  " +
+                sPattern[i]
+            );
         }
     }
 
-    // Static method to build 'O'
+    static class CharacterPatternMap {
+
+        private char character;
+        private String[] pattern;
+
+        public CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+
     static String[] buildO() {
         return new String[] {
             " ***** ",
@@ -25,7 +49,6 @@ public class OOPSBannerApp {
         };
     }
 
-    // Static method to build 'P'
     static String[] buildP() {
         return new String[] {
             " ***** ",
@@ -38,7 +61,6 @@ public class OOPSBannerApp {
         };
     }
 
-    // Static method to build 'S'
     static String[] buildS() {
         return new String[] {
             " ***** ",
