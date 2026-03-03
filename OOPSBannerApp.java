98a9893565aeb1ddbeed134fb60e2109a61ff5d3
prasanna-sp07 <prasanna.sp2540@gmail.com>
2026-03-03 21:19:05 +0530

    Added UC6

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,53 @@
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] bannerLines = {
-            String.join("", "  *****    ", " *****    ", " *****    ", " ***** "),
-            String.join("", " *     *  ", " *     *  ", " *     *  ", " *      "),
-            String.join("", " *     *  ", " *     *  ", " *     *  ", " *      "),
-            String.join("", " *     *  ", " *     *  ", " *****   ", " ***** "),
-            String.join("", " *     *  ", " *     *  ", " *       ", "      *"),
-            String.join("", " *     *  ", " *     *  ", " *       ", "      *"),
-            String.join("", "  *****   ", " *****   ", " *       ", " ***** ")
-        };
+        String[] o = buildO();
+        String[] p = buildP();
+        String[] s = buildS();
 
-        for (String line : bannerLines) {
-            System.out.println(line);
+        for (int i = 0; i < 7; i++) {
+            System.out.println(o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i]);
         }
+    }
+
+    // Static method to build 'O'
+    static String[] buildO() {
+        return new String[] {
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    // Static method to build 'P'
+    static String[] buildP() {
+        return new String[] {
+            " ***** ",
+            "*     *",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    // Static method to build 'S'
+    static String[] buildS() {
+        return new String[] {
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        };
     }
 }
